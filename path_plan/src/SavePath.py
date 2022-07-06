df36458e6b25a8b326ade10b932e89c08092b54e
agbread <jeoug1002@gmail.com>
2022-07-06 19:43:26 +0900

    existed path pub

--- path_plan/src/SavePath.py
+++ path_plan/src/SavePath.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-from ast import Load
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from tf.transformations import euler_from_quaternion
 import rospy
 from DB import *
 from std_msgs.msg import Empty, String, UInt8
 from path_plan.msg import PathRequest, PathResponse
-from geometry_msgs.msg import PoseArray, Pose, PoseStamped
+from geometry_msgs.msg import PoseArray, Pose
 from nav_msgs.msg import Path
 from LoadPath import LoadPath
 
@@ -55,45 +54,13 @@ class SavePath():
         rospy.logwarn(len(path.cx))
         return path
 
-    def bringPath(self):
-        # bring path
-        self.path_id = db.bring_path_id(
-            self.Request.start, self.Request.end)
-        self.path_info = db.bring_pathinfo(self.path_id)
-        # [[x, y, yaw]]
-
-    def toRosPath(self):
-        # ros path publish
-        self.path = Path()
-        self.path.header.frame_id = "map"
-        self.path.header.stamp = rospy.Time.now()
-
-        for info in self.path_info:
-
-            pose = PoseStamped()
-
-            pose.header.frame_id = "map"
-            pose.header.stamp = rospy.Time.now()
-
-            pose.pose.position.x = info[0]
-            pose.pose.position.y = info[1]
-
-            quat = quaternion_from_euler(0., 0., info[2])
-
-            pose.pose.orientation.x = quat[0]
-            pose.pose.orientation.y = quat[1]
-            pose.pose.orientation.z = quat[2]
-            pose.pose.orientation.w = quat[3]
-
-            self.path.poses.append(pose)
-
 
 if __name__ == "__main__":
     rospy.init_node("SavePath")
 
     db = DB()
     save_path = SavePath()
-    # load_path = LoadPath(db)
+    load_path = LoadPath(db)
 
     rospy.Subscriber("/PathPoint", PathRequest,
                      callback=save_path.PathPointCallback)
@@ -102,7 +69,7 @@ if __name__ == "__main__":
 
     check_pub = rospy.Publisher("/saving_check", String, queue_size=1)
     path_pub = rospy.Publisher("/reset_path", Empty, queue_size=1)
-    existing_path_pub = rospy.Publisher("/existing_path", Path, queue_size=1)
+    existed_path_pub = rospy.Publisher("/existed_path", Path, queue_size=1)
 
     r = rospy.Rate(10)
     while not rospy.is_shutdown():
@@ -113,10 +80,11 @@ if __name__ == "__main__":
             # save path
             if flag == 1:
                 # already existed
-                # existing path pub
-                save_path.bringPath()
-                save_path.toRosPath()
-                existing_path_pub.publish(save_path.path)
+                # existed path pub
+                load_path.Request = save_path.Request
+                load_path.bringPath()
+                load_path.toRosPath()
+                existed_path_pub.publish(load_path.path)
 
                 check_pub.publish(
                     'Path is already existed\nDo you want to overwrite data?')
