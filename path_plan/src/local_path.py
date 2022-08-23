6885dd05c1c454667a5e021f9e82bb98a44ac685
agbread <jeoug1002@gmail.com>
2022-08-23 17:29:13 +0900

    before pull

--- path_plan/src/local_path.py
+++ path_plan/src/local_path.py
@@ -41,8 +41,22 @@ class LocalPath():
         self.goal_x = dx[target_idx]
         self.goal_y = dx[target_idx]
     
-    def generate_trajectory(self):
-        pass
+    def cal_range(self, a, b, d):
+        try:
+            x1 = (-(a*b - self.goal_x - self.goal_y*a) + m.root((a*b - self.goal_x - self.goal_y*a)**2 - (a+1)*(self.goal_x**2 + b**2 - 2*self.goal_y*b + self.goal_y**2 -d**2)))/(a+1)
+            x2 = (-(a*b - self.goal_x - self.goal_y*a) - m.root((a*b - self.goal_x - self.goal_y*a)**2 - (a+1)*(self.goal_x**2 + b**2 - 2*self.goal_y*b + self.goal_y**2 -d**2)))/(a+1)
+
+        
+        except :
+            print(x)
+    def generate_trajectory(self, state):
+        self.calc_target_index(state)
+
+        current_goal_l = np.polyfit([self.goal_x, state.x], [self.goal_y, state.y], 1)
+        vertical_a = (-1)/current_goal_l[0]
+        vertical_b = ((self.goal_y + state.y)/2)-vertical_a*((self.goal_x +state.x)/2) 
+
+
 
 if __name__ == "__main__":
     r = rospy.Rate(10)
@@ -56,5 +70,5 @@ if __name__ == "__main__":
     rospy.Subscriber("/path_response", PathResponse, path.pathCallback)
 
     while not rospy.is_shutdown():
-
+        path.generate_trajectory(state)
         r.sleep()
