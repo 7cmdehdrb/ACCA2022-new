aa2fc915aec6495d47a143d7efd6c8060df19fa7
7cmdehdrb <7cmdehdrb@naver.com>
2022-09-29 20:57:12 +0900

    horizontal parking end : except parking area selection

--- parking/src/horizontal_parking2.py
+++ parking/src/horizontal_parking2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from time import sleep
 import rospy
 import rospkg
 import numpy as np
@@ -10,8 +9,9 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from parking_area_selector import ParkingAreaSelector
 from parking_area import ParkingArea
 from cubic_spline_planner import calc_spline_course
-from reeds_shepp_path_planning import reeds_shepp_path_planning
+from load_parking_area import loadCSV
 from enum import Enum
+from time import sleep
 
 # msgs
 from std_msgs.msg import *
@@ -20,7 +20,6 @@ from nav_msgs.msg import *
 from visualization_msgs.msg import *
 from path_plan.msg import *
 from erp42_control.msg import *
-from load_parking_area import loadCSV
 
 try:
     erp42_control_pkg_path = rospkg.RosPack().get_path("erp42_control") + "/src"
@@ -34,11 +33,16 @@ except Exception as ex:
 class ParkingState(Enum):
     NONE = 0
     SEARCH = 1
-    ALIGN1 = 2
-    ALIGN2 = 3
-    PARKING1 = 4
-    PARKING2 = 5
-    PARKING3 = 6
+    BACK = 2
+    ALIGN1 = 3
+    ALIGN2 = 4
+    PARKING1 = 5
+    PARKING2 = 6
+    PARKING3 = 7
+    HOMING = 8
+    END1 = 9
+    END2 = 10
+    OUT = 11
 
     def __int__(self):
         return self.value
@@ -53,6 +57,7 @@ class HorizontalParking(object):
             rospy.get_param("/horizontal_parking/file", "hor_parking.csv")
         self.parking_areas = loadCSV(file)
         self.parking_idx = 0
+        self.target_area = self.parking_areas[self.parking_idx]
 
         self.parking_state = ParkingState.ALIGN1
         self.target_idx = 0
@@ -61,15 +66,11 @@ class HorizontalParking(object):
 
         # ETC
         self.cmd_pub = cmd_pub
-        self.cmd_msg = ControlMessage()
         self.parking_pub = rospy.Publisher(
             "/horizontal_parking/parking_area", MarkerArray, queue_size=5
         )
         self.path_pub = rospy.Publisher(
             "/horizontal_parking/path", Path, queue_size=1
-        )
-        self.test_pub = rospy.Publisher(
-            "/horizontal_parking/test", PointStamped, queue_size=1
         )
 
         for i in range(10):
@@ -91,14 +92,12 @@ class HorizontalParking(object):
                 last_time = current_time
                 return 0
 
-            self.cmd_msg = ControlMessage(0, 0, 2, 0, 0, brake, 0)
-            self.cmd_pub.publish(self.cmd_msg)
+            self.cmd_pub.publish(ControlMessage(0, 0, 2, 0, 0, brake, 0))
 
             r.sleep()
 
     def control(self):
         msg = ControlMessage()
-
         reverse = False
 
         if self.parking_state == ParkingState.NONE:
@@ -118,12 +117,12 @@ class HorizontalParking(object):
 
             if self.target_idx >= len(self.alignPath1.cx) - 1:
                 self.target_idx = 0
-                self.wait_for_stop(duration=3, brake=50)
+                self.wait_for_stop(duration=1, brake=90)
                 self.parking_state = ParkingState.ALIGN2
 
         elif self.parking_state == ParkingState.ALIGN2:
             reverse = True
-            msg.Speed = int(3)
+            msg.Speed = int(5)
 
             di, self.target_idx = self.stanely.stanley_control(self.state, self.alignPath2.cx,
                                                                self.alignPath2.cy, self.alignPath2.cyaw, self.target_idx, reverse=reverse)
@@ -134,7 +133,7 @@ class HorizontalParking(object):
 
             if self.target_idx >= len(self.alignPath2.cx) - 1:
                 self.target_idx = 0
-                self.wait_for_stop(duration=3, brake=50)
+                self.wait_for_stop(duration=1, brake=70)
                 self.parking_state = ParkingState.PARKING1
 
         elif self.parking_state == ParkingState.PARKING1:
@@ -143,9 +142,8 @@ class HorizontalParking(object):
 
             di = 30
 
-            target_area = self.parking_areas[self.parking_idx]
             _, _, parking_yaw = euler_from_quaternion(
-                [target_area.orientation.x, target_area.orientation.y, target_area.orientation.z, target_area.orientation.w])
+                [self.target_area.orientation.x, self.target_area.orientation.y, self.target_area.orientation.z, self.target_area.orientation.w])
 
             heading_vec = np.array(
                 [m.cos(self.state.yaw), m.sin(self.state.yaw)])
@@ -154,28 +152,24 @@ class HorizontalParking(object):
             theta = abs(m.acos(np.dot(heading_vec, parking_vec)))
 
             if m.radians(45) - theta < m.radians(5):
-                self.wait_for_stop(5)
                 self.parking_state = ParkingState.PARKING2
 
         elif self.parking_state == ParkingState.PARKING2:
             reverse = True
-            msg.Speed = int(3)
+            msg.Speed = int(5)
 
             di = 0
 
-            target_area = self.parking_areas[self.parking_idx]
             _, _, parking_yaw = euler_from_quaternion(
-                [target_area.orientation.x, target_area.orientation.y, target_area.orientation.z, target_area.orientation.w])
+                [self.target_area.orientation.x, self.target_area.orientation.y, self.target_area.orientation.z, self.target_area.orientation.w])
 
             area_reference_point = Point(
-                target_area.position.x +
-                (target_area.scale.x / 2.0) * m.cos(parking_yaw),
-                target_area.position.y +
-                (target_area.scale.x / 2.0) * m.sin(parking_yaw),
+                self.target_area.position.x +
+                (self.target_area.scale.x / 2.0) * m.cos(parking_yaw),
+                self.target_area.position.y +
+                (self.target_area.scale.x / 2.0) * m.sin(parking_yaw),
                 0.0
             )
-
-            self.publishPoint(area_reference_point)
 
             car_reference_point = Point(
                 self.state.x + 2.3 * m.cos(self.state.yaw),
@@ -190,10 +184,8 @@ class HorizontalParking(object):
             parking_vec = np.array([m.cos(parking_yaw), m.sin(parking_yaw)])
 
             dot = np.dot(car_vec, parking_vec)
-            print(dot)
 
             if dot < 0:
-                self.wait_for_stop(5)
                 self.parking_state = ParkingState.PARKING3
 
         elif self.parking_state == ParkingState.PARKING3:
@@ -202,9 +194,8 @@ class HorizontalParking(object):
 
             di = -30
 
-            target_area = self.parking_areas[self.parking_idx]
             _, _, parking_yaw = euler_from_quaternion(
-                [target_area.orientation.x, target_area.orientation.y, target_area.orientation.z, target_area.orientation.w])
+                [self.target_area.orientation.x, self.target_area.orientation.y, self.target_area.orientation.z, self.target_area.orientation.w])
 
             heading_vec = np.array(
                 [m.cos(self.state.yaw), m.sin(self.state.yaw)])
@@ -213,12 +204,73 @@ class HorizontalParking(object):
             theta = abs(m.acos(np.dot(heading_vec, parking_vec)))
 
             if theta < m.radians(5):
+                self.wait_for_stop(1)
+                self.parking_state = ParkingState.HOMING
+
+        elif self.parking_state == ParkingState.HOMING:
+            msg.Speed = int(3)
+
+            _, _, parking_yaw = euler_from_quaternion(
+                [self.target_area.orientation.x, self.target_area.orientation.y, self.target_area.orientation.z, self.target_area.orientation.w])
+
+            di, _ = self.stanely.stanley_control(
+                self.state, [0.0, self.target_area.position.x], [0.0, self.target_area.position.y], [0.0, parking_yaw], 1, reverse=reverse)
+
+            car_vec = np.array([self.state.x - self.target_area.position.x,
+                               self.state.y - self.target_area.position.y])
+            parking_vec = np.array([m.cos(parking_yaw), m.sin(parking_yaw)])
+
+            dot = np.dot(car_vec, parking_vec)
+
+            if dot > 0:
+                self.wait_for_stop(10)
+                self.parking_state = ParkingState.END1
+
+        elif self.parking_state == ParkingState.END1:
+            reverse = True
+            msg.Speed = int(3)
+
+            di = 0
+
+            dist = np.hypot(self.state.x - self.target_area.position.x,
+                            self.state.y - self.target_area.position.y)
+
+            if dist > 0.7:
+                self.wait_for_stop(1)
+                self.parking_state = ParkingState.END2
+
+        elif self.parking_state == ParkingState.END2:
+            msg.Speed = int(5)
+
+            di = 30
+
+            _, _, parking_yaw = euler_from_quaternion(
+                [self.target_area.orientation.x, self.target_area.orientation.y, self.target_area.orientation.z, self.target_area.orientation.w])
+
+            heading_vec = np.array(
+                [m.cos(self.state.yaw), m.sin(self.state.yaw)])
+            parking_vec = np.array([m.cos(parking_yaw), m.sin(parking_yaw)])
+
+            theta = abs(m.acos(np.dot(heading_vec, parking_vec)))
+
+            if m.radians(45) - theta < m.radians(5):
+                self.parking_state = ParkingState.OUT
+
+        elif self.parking_state == ParkingState.OUT:
+            msg.Speed = int(5)
+
+            di = 0
+
+            dist = np.hypot(self.state.x - self.target_area.position.x,
+                            self.state.y - self.target_area.position.y)
+
+            if dist > 5.0:
                 self.wait_for_stop(5)
                 self.parking_state = ParkingState.NONE
 
         di = np.clip(di, -m.radians(30), m.radians(30))
         msg.Steer = int(m.degrees(di * (-1.0 if reverse is False else 1.0)))
-        msg.Gear = 2 if reverse is False else 1
+        msg.Gear = 2 if reverse is False else 0
 
         self.cmd_pub.publish(msg)
 
@@ -306,7 +358,7 @@ class HorizontalParking(object):
 if __name__ == "__main__":
     rospy.init_node("horizontal_parking")
 
-    state = State(odometry_topic="/ndt_matching/ndt_pose", hz=30, test=False)
+    state = State(odometry_topic="/odometry/kalman", hz=30, test=True)
     stanley = Stanley()
 
     cmd_pub = rospy.Publisher(
